f6370e25dae16b643e56f04afa9770593f666dad
Jarukit Bunchan <jarukit.bunchan@gmail.com>
2025-06-20 15:49:30 +0700

    fix: change console.debug to console.log for clearer seeding outputte

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@/src/libs/prisma'
 
 async function seedData() {
-  console.debug('Seeding data...')
+  console.log('Seeding data...')
   const testData = await prisma.test.findMany()
   console.debug('testData', testData)
   // await testSeed()
